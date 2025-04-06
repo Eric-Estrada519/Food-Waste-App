f6e8b1999b97b85c42faacfe28d177ad63e73fed
Eric Estrada <erice4@uci.edu>
2025-04-06 13:25:56 -0700

    Start of the app

---
+++ fresh-fetch/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fresh Fetch',
+  webDir: 'www'
+};
+
+export default config;
